bd202e033a6bd15320a33da7b6b6fe4d3c35d79e
vanhhn <vietanhvmo@gmail.com>
2024-05-04 16:48:24 +0700

    ok

--- CON9_03.cpp
+++ CON9_03.cpp
@@ -1,58 +1,23 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-typedef pair<int, int> p;
-typedef vector<vector<int> > vt;
-typedef vector<pair<int, int> > vp;
-
-const ll mod = 1e9 + 7;
-const int oo = 1e6 + 7;
-
-#define f first
-#define s second
-#define pb push_back
-#define ep emplace_back
-#define sz(a) (int) a.size()
-#define ms(s, n) memset(s, n, sizeof(s))
-#define present(t, x) (t.find(x) != t.end())
-#define all(a) (a.begin(), a.end())
-#define For(i, l, r) for (int i = l; i <= r; i++)
-#define Fod(i, r, l) for (int i = r; i >= l; i--)
-#define fillchar(a, x) memset(a, x, sizeof (a))
-#define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-void FileIO(){
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-}
-
-
-int a[1001][1001];
-vector<int> adj[1001];
+#define ll long long
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+     int n;
+     cin>>n;
+     int a[n+1][n+1];
+     for(int i=1;i<=n;i++){
+     	for(int j=1;j<=n;j++){
+     		cin>>a[i][j];
+		 }
+	 }
+	 for(int i=1;i<=n;i++){
+     	for(int j=1;j<=n;j++){
+     		if(a[i][j]){
+     			cout<<j<<" ";
+			 }
+		 }
+		 cout<<"\n";
+	 }
 
-    int n; cin >> n; 
-    for(int i = 1; i <= n; i ++){
-        for(int j = 1; j <= n; j++) cin >> a[i][j];
-    }
-    for(int i = 1; i <= n; i ++){
-        for(int j = 1; j <= n; j++){
-            if(a[i][j]){
-                adj[i].push_back(j);
-            }
-        }
-    }
-    for(int i = 1; i <= n; i ++){
-        for(auto x: adj[i])
-            cout << x << " ";
-        cout << endl;
-    }
-    return 0;
+   return 0;
 }
